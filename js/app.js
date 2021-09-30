67221b675b9dc948ac54565f0ffe09378bc18daa
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-10-01 00:14:03 +0600

    01.10.21

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// getting DOM value of Shopping cart by cartDetails function
+		// getting DOM value of Shopping cart by cartDetails function
 		function cartDetails($btn){
 			// getting cart id
 			let id = $btn.parents('#mainCart').attr('class');
@@ -40,24 +40,67 @@
 
 		function update_fontawesome_cart_icon(){
 			let qty = 0;
+			let total_cart_money = 0;
+			let cart_item_arry = [];
+			let count = 0;
 			if(Object.keys(localStorage).length){
 				let itemKeys = Object.keys(localStorage);
 				$.each(itemKeys,function(key,value){
 					let itemValues = localStorage.getItem(value);
 					let items = JSON.parse(itemValues);
+					total_cart_money += items[2]*items[3];
+					cart_item_arry[count] = [key, items[0], items[1], items[2], items[3]];
 					qty += items[3]; 
+					count++;
 				})
 			}
 
 			// ADD TOTAL QTY BUYER ADD TO CART ICON
 			$('#num').text(qty);
-			return qty;
+			return [qty, total_cart_money, cart_item_arry];
+		}
+
+		function display_cart(){
+			$('#mainappend').remove();
+			let html = "";
+			html += `<div id="mainappend">`;
+			let cart_data = update_fontawesome_cart_icon();
+			cart_data[2].forEach(function(item){
+				html += `<div id="cartMiddle">
+					<div id="imageNamePrice">
+						<div id="cartImage">
+							<img src="${item[1]}">
+						</div>
+						<div id="namePrice">
+							<p>${item[2]}</p>
+							<h3> $ ${item[3]} </h3>
+							<button>Remove</button>
+						</div>
+					</div>
+					<div id="fontAwesome">
+						<i class="fas fa-chevron-up"></i>
+						<p id="cart_qty">${item[4]}</p>
+						<i class="fas fa-chevron-down"></i>
+					</div>
+				</div>`
+			});
+			html += `<p id="total">Cart Total : $ <span>${cart_data[1]}</span></p>
+			</div>`;
+			return html;
 		}
 
 		let num = update_fontawesome_cart_icon();
 
+		console.log(num);
+
 		// WHEN USER REFRESH THE BROWSER
-		$('#num').text(num);
+		$('#num').text(num[0]);
+
+		const content = $(document.body);
+
+		content.on('click', '.fa-chevron-up',function(){
+			
+		})
 
 		// when customer click 'Add to cart' button
 		$('.add_to_cart').on('click',function(){
@@ -73,5 +116,6 @@
 
 		//when user click cart icon
 		$('#cartIcon').on('click',function(){
+			$('#append').append(display_cart());
 			$('#shopCart').css('transform','translateX(0%)')
 		})
