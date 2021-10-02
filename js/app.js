bf651302d93ce2a481e541a656bf9f341f3700bd
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-10-03 00:28:57 +0600

    03.10.21

--- js/app.js
+++ js/app.js
@@ -17,23 +17,23 @@
 		}
 
 		// customer 'Add to cart' button clicked cart details save to localstorage
-		function save_To_localstorage(key,values){
-			if(localStorage.getItem(key)!=null){
-				let secondClicked = localStorage.getItem(key);
+		function save_To_localstorage(item){
+			if(localStorage.getItem(item[0])!=null){
+				let secondClicked = localStorage.getItem(item[0]);
 				// localStorage.getItem give key values as string.we need array here.so we use JSON.parse function to have array
 				let actualData = JSON.parse(secondClicked);
 				let qty = actualData[3];
 				qty++;
 				let data = JSON.stringify([actualData[0],actualData[1],actualData[2],qty]);
-				localStorage.setItem(key,data);
+				localStorage.setItem(item[0],data);
 				update_fontawesome_cart_icon();
 			}else{
 				let qty = 1;
-				let customer_clicked_cart_array = [values[1],values[2],values[3],qty];
+				let customer_clicked_cart_array = [item[1],item[2],item[3],qty];
 
 				// localStorage save item as key value pair. value is saved as string here.so in order to have array from localStorage.we need to convert array to string by stringify function
 				let data = JSON.stringify(customer_clicked_cart_array);
-				localStorage.setItem(key,data);
+				localStorage.setItem(item[0],data);
 				update_fontawesome_cart_icon();
 			}
 		}
@@ -99,13 +99,13 @@
 		const content = $(document.body);
 
 		content.on('click', '.fa-chevron-up',function(){
-			alert('ok');
+			
 		})
 
 		// when customer click 'Add to cart' button
 		$('.add_to_cart').on('click',function(){
 			let itemDetails = cartDetails($(this));
-			save_To_localstorage(itemDetails[0],itemDetails);
+			save_To_localstorage(itemDetails);
 		})
 
 
